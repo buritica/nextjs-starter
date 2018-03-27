c0212de6ffdefc70994db6b6330a8f44fc2b480d
Juan Pablo Buriticá <buritica@gmail.com>
2018-03-26 22:43:48 -0400

    [restore] tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,12 @@ import renderer from 'react-test-renderer'
 import App from '../pages/index.js'
 
 describe('With Enzyme', () => {
+  it('Index shows "Starter', () => {
+    const app = shallow(<App />)
+
+    expect(app.find('h2').text()).toEqual('Starter')
+  })
+
   it('Index shows "☠️hello world ☠️"', () => {
     const app = shallow(<App />)
 
